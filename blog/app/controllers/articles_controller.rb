0102fd56678cb4d202770aa6984ede5454d90e68
EC2 Default User <ec2-user@ip-172-31-71-78.ec2.internal>
2021-11-08 17:00:23 +0000

    speed run

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -47,3 +47,11 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :text)
     end
 end
+
+class ArticlesController < ApplicationController
+ 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -15,3 +15,9 @@ end
 resources :articles do
   resources :comments
 end
+
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
